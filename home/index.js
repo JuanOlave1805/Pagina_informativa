3438b185bdb33cd3fa130265adaccbf48b12d90e
JuanOlave1805 <olavejuan1805@gmail.com>
2023-11-27 08:59:10 -0500

    Prueba 1 avance 1

--- home/index.js
+++ home/index.js
@@ -7,6 +7,14 @@ function closeNav(){
 }
 
 window.addEventListener("scroll", function(){
-    var header = document.querySelector(".Header__Box");
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0);
+  })
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".Header__Link");
+    header.classList.toggle("abajo",window.scrollY>0);
+  })
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".Header__Link--edit");
     header.classList.toggle("abajo",window.scrollY>0);
   })
