4819cf484a66c1d2ac3962753052fe617a478e1a
JuanOlave1805 <olavejuan1805@gmail.com>
2023-12-03 20:01:51 -0500

    Terminado 1

--- home/index.js
+++ home/index.js
@@ -1,26 +1,34 @@
-function openNav(){
-    document.getElementById("Menu__Mobile").style.width = "100%";
-}
-
-function closeNav(){
-    document.getElementById("Menu__Mobile").style.width = "0%";
-}
-
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
   })
 window.addEventListener("scroll", function(){
-    var header = document.querySelector(".Header__Box");
+    var header = document.querySelector(".header__Box");
     header.classList.toggle("abajo",window.scrollY>0);
   })
 window.addEventListener("scroll", function(){
-    var header = document.querySelector(".Header__Link");
+    var header = document.querySelector(".header__Link");
     header.classList.toggle("abajo",window.scrollY>0);
   })
 window.addEventListener("scroll", function(){
-    var header = document.querySelector(".Header__Link--edit");
+    var header = document.querySelector(".header__Link--edit");
     header.classList.toggle("abajo",window.scrollY>0);
   })
 
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén todas las tarjetas por su clase o cualquier otro selector
+  var cards = document.querySelectorAll(".card");
+
+  // Agrega un manejador de clic a cada tarjeta
+  cards.forEach(function (card) {
+    card.addEventListener("click", function () {
+      // Alternar la clase 'flipped' en la tarjeta clicada
+      card.classList.toggle("flipped");
+    });
+  });
+});
