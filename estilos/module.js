5bec1473d4bb3dc0581e45a811af2ba7ce50e92c
Yariangel <gabrielamunar4@gmail.com>
2024-06-24 16:51:25 -0500

    Terminando noticias, faltan 5

--- estilos/module.js
+++ estilos/module.js
@@ -1,8 +1,93 @@
 export const data = [
-    {
-        titulo: "Revolucione su proceso de codificación con ChatGPT: su mejor compañero para desarrolladores",
-        img: "../Img noticias/noticias mundo/27 noticia.png",
-        paragraph: "En un mundo donde la innovación y la eficiencia son clave, ¿qué tal tener a tu disposición un asistente que no solo comprende tus necesidades de programación, sino que las ejecuta con precisión? ChatGPT, el revolucionario modelo de inteligencia artificial de OpenAI, está aquí para transformar la forma en que desarrollas software. Desde escribir y refactorizar código hasta explicar complejas funcionalidades, esta herramienta avanzada no solo mejora la productividad, sino que también impulsa la calidad y la claridad de tus proyectos. Descubre cómo ChatGPT puede elevar tu desarrollo a nuevas alturas, optimizando tu tiempo y liberándote para enfocarte en lo que más importa: la creatividad y la innovación en la programación. Sumérgete en una experiencia donde la tecnología se encuentra con la intuición, y cada línea de código se convierte en una obra maestra de eficiencia y funcionalidad.",
-        url: "https://dev.to/techiesdiary/chatgpt-prompts-for-developers-216d?ref=dailydev  "
-    }
+  {
+    titulo: "5 funciones interesantes de Chrome DevTools que la mayoría de los desarrolladores no conocen",
+    img: "../Img noticias/noticias mundo/chrome.jpg",
+    paragraph: "Descubre cómo maximizar Chrome DevTools más allá de sus funciones básicas con estas características avanzadas y menos conocidas. Desde observar valores variables en tiempo real hasta puntos de interrupción condicionales y registros conservados, estas herramientas te permiten depurar más rápido y optimizar el rendimiento de tu sitio web de manera efectiva. Aprende a utilizar estas funciones para mejorar tu flujo de trabajo y aumentar tu productividad como desarrollador. No te pierdas las gemas ocultas de Chrome DevTools que pueden transformar tu experiencia de desarrollo.",
+    url: "https://javascript.plainenglish.io/5-cool-chrome-devtools-features-most-developers-dont-know-about-cf55d3b46c95"
+  },
+  {
+    titulo: "Lo último en CSS y UI web: resumen de I/O 2024",
+    img: "../Img noticias/noticias mundo/css.webp",
+    paragraph: "La evolución de CSS está transformando la web con nuevas funcionalidades que permiten crear experiencias interactivas y dinámicas. Desde animaciones basadas en desplazamiento que guían al usuario a través de historias visuales hasta transiciones fluidas entre vistas de páginas, estas innovaciones están redefiniendo cómo interactuamos en la web. Descubre cómo estas herramientas están siendo utilizadas por grandes nombres como Airbnb y GitHub para mejorar la navegación y la usabilidad. No te pierdas las últimas actualizaciones presentadas en Google I/O 2024 y prepárate para explorar el futuro de la interfaz de usuario web.",
+    url: "https://developer.chrome.com/blog/new-in-web-ui-io-2024?ref=dailydev#scroll-driven-animation-visual"
+    //estados--token--tipo autenticacion--paso paso de api-- consultar conexion api de marvel
+  },
+  {
+    titulo: "Los 10 Mandamientos del Código Impecable: Reglas Esenciales para Escribir Código de Calidad",
+    img: "../Img noticias/noticias mundo/clear_code.webp",
+    paragraph: "El libro 'Código limpio' de 'Tío Bob' es una guía esencial para todo desarrollador que busca perfeccionar su arte. Con enfoque en nombres descriptivos y la eliminación de comentar ios superfluos, promueve la claridad y la legibilidad del código. Desde la adopción de estándares de diseño hasta la simplicidad de KISS, y el uso de excepciones para manejar errores, cada principio apunta a optimizar la eficiencia y mantenibilidad del software. Descubre cómo estas reglas transforman la forma en que desarrollamos, manteniendo siempre el código en su mejor estado.",
+    url: "https://blog.stackademic.com/top-10-clean-code-rules-831fb34caff7"
+  },
+  {
+    titulo: "CSS Bud, ¿será el mejor lugar para obtener fragmentos y herramientas CSS?",
+    img: "../Img noticias/noticias mundo/css.png",
+    paragraph: "CSS Bud ofrece una colección excepcional de herramientas y fragmentos CSS de alta calidad, ideales para satisfacer tus necesidades creativas. Descubre una variedad de recursos que te ayudarán a mejorar el diseño y funcionalidad de tus proyectos web de manera efectiva. Desde animaciones y diseños responsivos hasta elementos de interfaz innovadores, CSS Bud te proporciona todo lo necesario para potenciar tu creatividad y optimizar tu flujo de trabajo de desarrollo. Explora cómo estas herramientas pueden transformar tus diseños y llevar tu desarrollo web al siguiente nivel.",
+    url: "https://cssbud.com/?ref=dailydev"
+  },
+  {
+    titulo: "Chart.js, libreria de JavaScript para graficos",
+    img: "../Img noticias/noticias mundo/chart.js.jpg",
+    paragraph: "Descubre cómo Chart.js revoluciona la visualización de datos con potentes gráficos JavaScript. Desde barras y líneas hasta tortas y radar, esta biblioteca te ofrece herramientas avanzadas para crear visualizaciones dinámicas y personalizadas. Aprende cómo integrar y personalizar gráficos en tu proyecto web para comunicar datos de manera efectiva y atractiva. Explora las características clave de Chart.js que impulsarán tus habilidades de desarrollo y mejorarán la experiencia del usuario.",
+    url: "https://www.chartjs.org/?ref=dailydev"
+  },
+  {
+    titulo: "Transiciones de vista sin JavaScript",
+    img: "../Img noticias/noticias mundo/JavaScript.png",
+    paragraph: "Descubre el futuro de Astro con la nueva API View Transitions, que permite transiciones nativas entre páginas sin JavaScript, directamente en el navegador. Chrome 126 y Microsoft Edge 126 han habilitado esta funcionalidad, ofreciendo una experiencia similar a la de las aplicaciones. Aprende cómo implementar estas transiciones en tu sitio web con una simple regla CSS y explora demostraciones en vivo para verlo en acción. Este avance promete simplificar el desarrollo web y mejorar la experiencia del usuario sin comprometer el rendimiento. No te pierdas esta innovación en la navegación web del futuro con Astro.",
+    url: "https://astro.build/blog/future-of-astro-zero-js-view-transitions/?ref=dailydev"
+  },
+  {
+    titulo: "Consejos profesionales para desarrolladores web",
+    img: "../Img noticias/noticias mundo/chrome.jpg",
+    paragraph: "Descubre cómo maximizar tu productividad como desarrollador web con ocho extensiones esenciales de Chrome y herramientas de DevTools. Desde Adblock Plus hasta herramientas especializadas como React Dev Tools y Redux Dev Tools, optimiza tu flujo de trabajo con consejos profesionales y técnicas avanzadas. Aprende a usar las herramientas de desarrollo para inspeccionar elementos, depurar JavaScript, monitorear rendimiento y más. Además, descubre trucos como configurar espacios de trabajo para editar código en tiempo real y utiliza atajos de teclado para ahorrar tiempo. ¡No te pierdas esta guía completa para acelerar tu desarrollo web en 2024!",
+    url: "https://levelup.gitconnected.com/chrome-devtools-extensions-and-pro-tips-for-web-developers-85e68b7c2722"
+  },
+  {
+    titulo: "¿Qué son las señales en Tailwind, cómo funcionan y casos utilizados?",
+    img: "../Img noticias/noticias mundo/Tailwind-CSS.jpeg",
+    paragraph: "Descubre las señales en Tailwind CSS: la clave para estilos web dinámicos y responsivos. Este artículo explora cómo las señales permiten aplicar clases de utilidad de manera condicional, optimizando tu flujo de desarrollo. Desde estilos responsivos hasta componentes personalizados, las señales ofrecen flexibilidad para crear diseños innovadores con mínima complejidad. Aprende cómo puedes potenciar tu proyecto web con ejemplos prácticos y fragmentos de código. ¡No te pierdas esta guía completa sobre las últimas innovaciones en diseño con Tailwind CSS!",
+    url: "https://blog.stackademic.com/what-are-signals-in-tailwind-how-do-they-work-and-use-1cf97929f540"
+  },
+  {
+    titulo: "Tendencias UX/UI en 2024",
+    img: "../Img noticias/noticias mundo/Diseno-UI-y-UX.png",
+    paragraph: "Descubre las tendencias de diseño que definirán el 2024: desde gráficos 3D que revolucionan la estética web hasta microanimaciones que guían la experiencia del usuario. Exploraremos cómo estas innovaciones están transformando desde simples páginas de destino hasta complejas plataformas digitales, ofreciendo nuevas formas de interactuar y crear conexiones significativas con los usuarios. Prepárate para sumergirte en un mundo visualmente cautivador y funcional. ¡No te pierdas las últimas predicciones de Polina Korotkaya sobre el futuro del diseño digital!",
+    url: "https://medium.com/@dev.family/ux-ui-trends-in-2024-84c98632c675"
+  },
+  {
+    titulo: "¿Será ZED el IDE nuevo, moderno, rápido y (recientemente) de código abierto el asesino de VSCode?",
+    img: "../Img noticias/noticias mundo/zed_ide.webp",
+    paragraph: "¡Descubre Zed, el nuevo IDE revolucionario que desafía a VSCode! Construido en Rust y diseñado para velocidad extrema con GPUI, supera a sus competidores en rendimiento y confiabilidad. Con características avanzadas como colaboración en tiempo real y asistencia de GitHub Copilot, Zed redefine la experiencia de programación. Aunque en versión beta, promete transformar el desarrollo de software. ¡Explora cómo Zed puede llevar tu codificación al siguiente nivel!",
+    url: "https://blog.stackademic.com/reviewing-zed-the-ide-of-the-future-305d681d990c"
+  },
+  {
+    titulo: "Diseños CSS para sitios web responsivos, una explicacion detallada de como crear sitios web reponsivos",
+    img: "../Img noticias/noticias mundo/reponse.png",
+    paragraph: "Descubre cómo los diseños CSS revolucionan la creación de sitios web responsivos. Desde Flexbox hasta CSS Grid, estos métodos modernos superan los desafíos de los antiguos diseños como el flujo normal y flotante. Aprende a optimizar la estructura y el atractivo visual de tus páginas web para diferentes dispositivos con nuestras mejores prácticas y soluciones a problemas comunes.",
+    url: "https://www.lambdatest.com/blog/css-layouts/?ref=dailydev "
+  },
+  {
+    titulo: "Tips de patrones de diseño de tablas de datos?",
+    img: "../Img noticias/noticias mundo/diseno.jpg",
+    paragraph: "Descubre cómo optimizar tus tablas de datos para mejorar la experiencia del usuario y la legibilidad. Desde alinear adecuadamente el texto y los números hasta utilizar filas de colores alternos y acciones interactivas, este artículo ofrece prácticas esenciales para diseñar tablas efectivas. ¡Transforma la forma en que tus usuarios interactúan con los datos y asegura una navegación fluida y eficiente!",
+    url: "https://bootcamp.uxdesign.cc/data-table-design-patterns-4e38188a0981"
+  },
+  {
+    titulo: "Mejoras en CSS, ¿será que no volveremos a usar SASS?",
+    img: "../Img noticias/noticias mundo/css.webp",
+    paragraph: "En 2024, CSS ha evolucionado rápidamente, integrando características una vez exclusivas de Sass como variables y anidamiento nativo. Esta transición promueve un flujo de trabajo más ágil y accesible, reduciendo la dependencia de herramientas adicionales como Sass para la mayoría de proyectos. Descubre cómo estas nuevas funcionalidades transforman el desarrollo web en nuestro artículo completo.",
+    url: "https://dev.to/karsten_biedermann/goodbye-sass-welcome-back-native-css-cf?ref=dailydev"
+  },
+  {
+    titulo: "16 prácticas para realizar un buen diseño web",
+    img: "../Img noticias/noticias mundo/tips de diseño.png",
+    paragraph: "Descubre cómo el diseño se ha democratizado gracias a herramientas y sistemas avanzados. Desde la optimización para distintos tipos de usuarios hasta consejos para claridad y accesibilidad, este artículo revela 16 prácticas esenciales para mejorar la experiencia del usuario y la eficacia empresarial. ¿Listo para elevar tu enfoque en diseño?",
+    url: "https://blog.logrocket.com/ux-design/16-practices-good-web-design/?ref=dailydev"
+  },
+  {
+    titulo: "Revolucione su proceso de codificación con ChatGPT: su mejor compañero para desarrolladores",
+    img: "../Img noticias/noticias mundo/27 noticia.png",
+    paragraph: "En un mundo donde la innovación y la eficiencia son clave, ¿qué tal tener a tu disposición un asistente que no solo comprende tus necesidades de programación, sino que las ejecuta con precisión? ChatGPT, el revolucionario modelo de inteligencia artificial de OpenAI, está aquí para transformar la forma en que desarrollas software. Desde escribir y refactorizar código hasta explicar complejas funcionalidades, esta herramienta avanzada no solo mejora la productividad, sino que también impulsa la calidad y la claridad de tus proyectos. Descubre cómo ChatGPT puede elevar tu desarrollo a nuevas alturas, optimizando tu tiempo y liberándote para enfocarte en lo que más importa: la creatividad y la innovación en la programación. Sumérgete en una experiencia donde la tecnología se encuentra con la intuición, y cada línea de código se convierte en una obra maestra de eficiencia y funcionalidad.",
+    url: "https://dev.to/techiesdiary/chatgpt-prompts-for-developers-216d?ref=dailydev  "
+  }
 ]
